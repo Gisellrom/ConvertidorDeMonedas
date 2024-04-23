b2ec250c8431ad56a8bba366d1391f38a6eab71c
Gisellrom <romero.giselln@gmail.com>
2024-04-23 10:53:15 -0600

    crea el Record de Moneda con sus atributos

--- src/com/aluracursos/desafio/conversion/Moneda.java
+++ src/com/aluracursos/desafio/conversion/Moneda.java
@@ -1,2 +1,8 @@
-package com.aluracursos.desafio.conversion;public class Moneda {
+package com.aluracursos.desafio.conversion;
+
+public record Moneda(
+        String monedaPK,
+        double monedaValorPK,
+        String cambioMoneda,
+        double cambioValor) {
 }
